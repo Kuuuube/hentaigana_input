02c72db4e6ec550608a64015b148dda71b48e6da
kuuuube <hexagonisalie@gmail.com>
2024-06-27 20:58:58 -0400

    Start egui transition

---
+++ src/gui.rs
@@ -0,0 +1,114 @@
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct HentaiganaInputGui {
+    text: String,
+    dark_mode: bool,
+    //#[serde(skip)] to opt-out of serialization of a field
+}
+
+impl Default for HentaiganaInputGui {
+    fn default() -> Self {
+        Self {
+            text: "".to_owned(),
+            dark_mode: true,
+        }
+    }
+}
+
+impl HentaiganaInputGui {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let mut fonts = egui::FontDefinitions::default();
+        fonts.font_data.insert(
+            "BabelStoneHan".into(),
+            egui::FontData::from_static(include_bytes!("../assets/BabelStoneHan.ttf")),
+        );
+        fonts
+            .families
+            .get_mut(&egui::FontFamily::Proportional)
+            .unwrap()
+            .push("BabelStoneHan".into());
+        cc.egui_ctx.set_fonts(fonts);
+
+        //restore state
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+
+impl eframe::App for HentaiganaInputGui {
+    //save state
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        set_theme(ctx, self.dark_mode);
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
+                    ui.menu_button("Menu", |ui| {
+                        light_dark_buttons(self, ui);
+
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                });
+
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                    unselectable_warn_if_debug_build(ui);
+                });
+            });
+        });
+
+        egui::SidePanel::right("right_sidepanel").show(ctx, |ui| {
+            for i in 1..=3 {
+                let sidebar_item = ui.selectable_label(false, format!("Button {}", i));
+                if sidebar_item.clicked() {
+                    println!("clicked {}", i);
+                }
+            }
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.add_sized(
+                    ui.available_size(),
+                    egui::TextEdit::multiline(&mut self.text).lock_focus(true),
+                );
+            });
+        });
+    }
+}
+
+fn unselectable_warn_if_debug_build(ui: &mut egui::Ui) {
+    if cfg!(debug_assertions) {
+        ui.add(
+            egui::Label::new(
+                egui::RichText::new("⚠ Debug build ⚠")
+                    .small()
+                    .color(ui.visuals().warn_fg_color),
+            )
+            .selectable(false),
+        );
+    }
+}
+
+fn light_dark_buttons(hentaigana_input_gui: &mut HentaiganaInputGui, ui: &mut egui::Ui) {
+    let mut visuals = ui.ctx().style().visuals.clone();
+    visuals.light_dark_radio_buttons(ui);
+    hentaigana_input_gui.dark_mode = visuals.dark_mode;
+    set_theme(ui.ctx(), visuals.dark_mode);
+}
+
+fn set_theme(ctx: &egui::Context, dark_mode: bool) {
+    if dark_mode {
+        ctx.set_visuals(egui::Visuals::dark());
+    } else {
+        ctx.set_visuals(egui::Visuals::light());
+    }
+}

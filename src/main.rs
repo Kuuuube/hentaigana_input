1b0acbb89a5e2e379bd3554b18a2c303a95ff964
kuuuube <hexagonisalie@gmail.com>
2024-06-28 16:07:26 -0400

    Fix sizing

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
+            .with_inner_size([650.0, 675.0])
+            .with_min_inner_size([650.0, 675.0])
             .with_icon(
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon.png")[..])
                     .expect("Failed to load icon"),

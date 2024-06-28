424e2627109a90e6d127029d470ab2de4e8a0a9d
kuuuube <hexagonisalie@gmail.com>
2024-06-27 23:37:50 -0400

    Add icon

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() -> eframe::Result<()> {
             .with_inner_size([400.0, 300.0])
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon.png")[..])
                     .expect("Failed to load icon"),
             ),
         ..Default::default()

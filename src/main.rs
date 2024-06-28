f3f8d176cf732477f56f8f732d0a4d01aa6260bb
kuuuube <hexagonisalie@gmail.com>
2024-06-28 19:46:49 -0400

    Add wasm deploy support

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod gui;
 mod hentaigana_dicts;
 mod normalize_input;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     env_logger::init();
 
@@ -25,4 +26,37 @@ fn main() -> eframe::Result<()> {
         native_options,
         Box::new(|cc| Box::new(gui::HentaiganaInputGui::new(cc))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let start_result = eframe::WebRunner::new()
+            .start(
+                "the_canvas_id",
+                web_options,
+                Box::new(|cc| Box::new(gui::HentaiganaInputGui::new(cc))),
+            )
+            .await;
+        let loading_text = eframe::web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        match start_result {
+            Ok(_) => {
+                loading_text.map(|e| e.remove());
+            }
+            Err(e) => {
+                loading_text.map(|e| {
+                    e.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    )
+                });
+                panic!("failed to start eframe: {e:?}");
+            }
+        }
+    });
 }
